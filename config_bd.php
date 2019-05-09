3a65622ffc69e3a0636da754f390a20ff662d9ac
NanoClem <clemdecoop.pro@gmail.com>
2019-05-09 11:41:42 +0200

    Gestion des templates

---
+++ config_bd.php
@@ -0,0 +1,7 @@
+<?php
+  define('DB_HOST', 'localhost');
+  define('DB_PORT', '3306');
+  define('DB_DATABASE', 'gestion_mesures');
+  define('DB_USERNAME', 'root');
+  define('DB_PASSWORD', 'root');
+ ?>
---
+++ login.php
@@ -0,0 +1,14 @@
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="login_style.css" />
+    <title>Formulaire</title>
+  </head>
+
+  <body>
+      <form class="box" method="post" name="formUSR" action="send.php">
+        <h1>Login</h1>
+        <input type="text" id="pseudo" name="pseudo" placeholder="pseudo" size="30" required />
+        <input type="password" id="passwd" name="passwd" placeholder="password" size="30" required />
+        <input type="submit" value="Send nudes" />
+      </form>
+  </body>
